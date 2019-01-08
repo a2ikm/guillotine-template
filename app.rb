973aaa6f19c010a5b77faf5e2a4b66af88e9dbbb
Masato Ikeda <masato.ikeda@gmail.com>
2019-01-09 03:09:57 +0900

    Rewrite CREATE TABLE with Sequel

--- app.rb
+++ app.rb
@@ -3,14 +3,10 @@ Bundler.require
 
 class App < Guillotine::App
   db = Sequel.connect(ENV["DATABASE_URL"])
-  db.run <<-SQL
-    CREATE TABLE IF NOT EXISTS `urls` (
-      `url` varchar(255) DEFAULT NULL,
-      `code` varchar(255) DEFAULT NULL,
-      UNIQUE KEY `url` (`url`),
-      UNIQUE KEY `code` (`code`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-  SQL
+  db.create_table?(:urls) do
+    column :url, String, unique: true
+    column :code, String, unique: true
+  end
 
   adapter = Guillotine::Adapters::SequelAdapter.new(db)
   set service: Guillotine::Service.new(adapter)
